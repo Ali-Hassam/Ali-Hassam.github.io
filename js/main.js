4615418f039fde2ae95d3825284bfc434b0a42ba
Hassam Ali <hassam.ms20@gmail.com>
2025-05-25 12:52:05 +0200

    Add hero component

--- js/main.js
+++ js/main.js
@@ -9,7 +9,14 @@ allElements.forEach((element) => {
     });
   });
 });
-
+// Typed JS
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
 document.querySelectorAll(".collapsible__header__CV").forEach((element) =>
   element.addEventListener("click", function () {
     this.parentElement.classList.toggle("collapsible--expanded");
